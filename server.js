ce96b6a0071da25691b46b348b065bfe920cd0c2
hollermay <udayanmoudgil@gmail.com>
2024-09-29 01:52:32 +0530

    added crud site, login/signup with logic,

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -9,12 +8,12 @@ app.use(express.static('public'));
 
 const data = require('./data.js');
 
-// Endpoint to fetch all movies
+
 app.get('/api/movies', (req, res) => {
   res.json(data.movies);
 });
 
-// Endpoint to fetch all reviews
+
 app.get('/api/reviews', (req, res) => {
   const allReviews = data.movies.flatMap(movie => movie.reviews.map(review => ({
     ...review,
@@ -23,26 +22,25 @@ app.get('/api/reviews', (req, res) => {
   res.json(allReviews);
 });
 
-// Endpoint to add a new review
+
 app.post('/api/reviews', (req, res) => {
   const { movieId, reviewer, reviewText, rating } = req.body;
 
-  // Check if movie exists
+
   const movie = data.movies.find(movie => movie.id == movieId);
   if (movie) {
-    // Create new review and add it to the movie
+  
     const newReview = { reviewer, reviewText, rating };
     movie.reviews.push(newReview);
     
-    // Respond with success
+
     return res.status(201).json({ message: 'Review added successfully!', review: newReview });
   }
 
-  // Respond with error if movie not found
   return res.status(404).json({ message: 'Movie not found!' });
 });
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
